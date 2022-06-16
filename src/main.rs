50841868a6c62808e10bd6309f319378e454e749
James Zow <jameszow@163.com>
2022-06-16 23:34:59 +0800

    add actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("ipfs demo");
+mod api;
+mod common;
+mod domain;
+mod service;
+
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[tokio::main] 
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/test", web::get().to(|| async { "server start success!" }))
+    })
+    .bind(("127.0.0.1", 8085))?
+    .run()
+    .await
 }
