d57db7a425913d856f2bb0ef888e40600ea57c4a
James Zow <jameszow@163.com>
2022-06-20 20:19:33 +0800

    update client version and log

--- covid-ipfs-client/app/src/main.rs
+++ covid-ipfs-client/app/src/main.rs
@@ -3,11 +3,28 @@
   windows_subsystem = "windows"
 )]
 
+mod cors;
+
+use tauri::{window::WindowBuilder, WindowUrl};
+
 fn main() {
-  let context = tauri::generate_context!();
+  let port = portpicker::pick_unused_port().expect("failed to find unused port");
 
   tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
-    .run(context)
+    .plugin(cors::Builder::new(3001).build())
+    .setup(move |app| {
+      WindowBuilder::new(
+        app,
+        "main".to_string(),
+        WindowUrl::External(format!("http://localhost:{}", 3001).parse().unwrap()),
+      )
+      .title("Covid IPFS")
+      .fullscreen(false)
+      .resizable(false)
+      .inner_size(850.0, 700.0)
+      .build()?;
+      Ok(())
+    })
+    .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
