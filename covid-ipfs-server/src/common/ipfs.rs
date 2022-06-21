9db6f9a14236df2e42e11ba946335622985b5a49
James Zow <jameszow@163.com>
2022-06-21 22:19:49 +0800

    Update ipfs.rs

--- covid-ipfs-server/src/common/ipfs.rs
+++ covid-ipfs-server/src/common/ipfs.rs
@@ -2,7 +2,7 @@ use ipfs_api_backend_actix::{IpfsClient, TryFromUri};
 
 pub fn coon() -> IpfsClient{
 
-    let client = IpfsClient::from_str("http://101.43.52.162:5001").unwrap();
+    let client = IpfsClient::from_str("http://127.0.0.1:5001").unwrap();
 
     client
 }
